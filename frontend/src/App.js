88455ea6772b10515e1a74d19c317003910e11e8
shrikant patil <patilshrikant0918@gmail.com>
2023-03-13 13:16:42 +0530

    aa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div className="grid-container">
+    <header className="row">
+      <div>
+        <a  className= "brand" href="index.html">amazona</a>
+      </div>
+      <div>
+        <a href="cart.html">cart</a>
+        <a href="Signin.html">Sign In </a>
+      </div>
+      
+    </header>
+    <main>
+      <div className="row center">
+        {
+          data.products.map(product =>(
+            <div className="card">
+          <a href="product.html"> <img className="medium" src="./images/p1.jpg" alt="product" /> </a>
+          <div className="card-body">
+            <a href="product.html"> <h2> {product.name } </h2> </a>
+            <div className="rating">
+              <span> <i className="fa fa-star"> </i> </span> 
+              <span> <i className="fa fa-star"> </i> </span> 
+              <span> <i className="fa fa-star"> </i> </span> 
+              <span> <i className="fa fa-star"> </i> </span> 
+              <span> <i className="fa fa-star"> </i> </span> 
+            </div>
+            <div className="price"> Rs120 </div>
+          </div>
+        </div>
+
+          )
+          )
+        }
+        
+        
+
+                  
+
+
+      </div>
+
+    </main>
+    <footer className="row center">
+      All right reserved
+      
+    </footer>
+  </div>
+
   );
 }
 
-export default App;
+
