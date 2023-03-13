359e508b96aa468c3abb6b890a6713d969c83967
shrikant patil <patilshrikant0918@gmail.com>
2023-03-13 16:28:16 +0530

    y

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 
 
 import React from 'react';
+import Product from './components/product';
 
 import data from './data';
 
@@ -11,33 +12,22 @@ function App() {
     <div className="grid-container">
     <header className="row">
       <div>
-        <a  className= "brand" href="index.html">amazona</a>
+        <a  className= "brand" href="/">amazona</a>
       </div>
       <div>
-        <a href="cart.html">cart</a>
-        <a href="Signin.html">Sign In </a>
+        <a href="/cart">cart</a>
+        <a href="/Signin">Sign In </a>
       </div>
       
     </header>
     <main>
+      <div>
       <div className="row center">
         {
-          data.products.map(product =>(
-            <div key={product._id}  className="card">
-          <a href={`/product/${product._id}`}> 
-          <img className="medium" src={product.image} alt="product" /> </a>
-          <div className="card-body">
-          <a href={`/product/${product._id}`}> <h2> {product.name } </h2> </a>
-            <div className="rating">
-              <span> <i className="fa fa-star"> </i> </span> 
-              <span> <i className="fa fa-star"> </i> </span> 
-              <span> <i className="fa fa-star"> </i> </span> 
-              <span> <i className="fa fa-star"> </i> </span> 
-              <span> <i className="fa fa-star"> </i> </span> 
-            </div>
-            <div className="price"> Rs120 </div>
-          </div>
-        </div>
+          data.products.map((product) =>(
+           <Product key={product._id} product={product}>
+
+           </Product>
 
           )
           )
@@ -48,6 +38,7 @@ function App() {
                   
 
 
+      </div>
       </div>
 
     </main>
